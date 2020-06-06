5f57f5b1f65343cb2a6504be22e83c54601d6a16
gregorytcheng <gregorytcheng@gmail.com>
2020-06-06 15:33:12 -0700

    Updates

---
+++ precache-manifest.8ad06e1d394857a562b1f275b5cbe9f1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d1ab1396fa1bd58380a1ad0adb4c1be",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6f1f638311edd33b9afb",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "aff6bad3808e7a8adcd4",
+    "url": "/static/js/2.11ba187d.chunk.js"
+  },
+  {
+    "revision": "4d47ffde3dc69784e8e23fd910817ef8",
+    "url": "/static/js/2.11ba187d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f1f638311edd33b9afb",
+    "url": "/static/js/main.b9fcc6cd.chunk.js"
+  },
+  {
+    "revision": "302d0934deb8f5146d63",
+    "url": "/static/js/runtime-main.ac1cdd6f.js"
+  }
+]);
