c0df20392879244d2c6186d5a2305953bb7a3114
gregorytcheng <gregorytcheng@gmail.com>
2020-06-06 15:36:17 -0700

    Updates

---
+++ precache-manifest.9824ba8b53f9a1dba7af7bc3eb25e1a1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed4e928b27bb681263497696825fa604",
+    "url": "/news-dashboard-react/index.html"
+  },
+  {
+    "revision": "aa99f5ea456a72ad2dc4",
+    "url": "/news-dashboard-react/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "aff6bad3808e7a8adcd4",
+    "url": "/news-dashboard-react/static/js/2.11ba187d.chunk.js"
+  },
+  {
+    "revision": "4d47ffde3dc69784e8e23fd910817ef8",
+    "url": "/news-dashboard-react/static/js/2.11ba187d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa99f5ea456a72ad2dc4",
+    "url": "/news-dashboard-react/static/js/main.5acf0a84.chunk.js"
+  },
+  {
+    "revision": "fccf6d47084fe6835007",
+    "url": "/news-dashboard-react/static/js/runtime-main.89328bfe.js"
+  }
+]);
