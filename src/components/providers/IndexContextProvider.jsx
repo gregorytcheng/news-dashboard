cb33daf480e8d0321d9fcb8166c2e47663473ac8
gregorytcheng <gregorytcheng@gmail.com>
2020-06-09 19:47:34 -0700

    Persisting index order in localstorage

--- src/components/providers/IndexContextProvider.jsx
+++ src/components/providers/IndexContextProvider.jsx
@@ -1,39 +1,14 @@
 import React, { useState, useEffect } from "react";
 import IndexContext from "../../contexts/IndexContext";
 import { getIndexData } from "../../services/NewsFeedService";
+import {
+  IndexOrderDefault,
+  INDEX_ORDER_LOCAL_STORAGE_KEY,
+} from "../../constants/IndexConstants";
 
 const IndexContextProvider = ({ children }) => {
   const [indexData, setIndexData] = useState(undefined);
-  const [indexOrder, setIndexOrder] = useState([
-    {
-      symbol: "GSPC",
-      country: "us",
-    },
-    {
-      symbol: "DJI",
-      country: "us",
-    },
-    {
-      symbol: "FTSE",
-      country: "uk",
-    },
-    {
-      symbol: "STOXX50E",
-      country: "eu",
-    },
-    {
-      symbol: "99001.SZ",
-      country: "cn",
-    },
-    {
-      symbol: "HSI",
-      country: "hk",
-    },
-    {
-      symbol: "N225",
-      country: "jp",
-    },
-  ]);
+  const [indexOrder, setIndexOrder] = useState(undefined);
 
   useEffect(() => {
     getIndexData()
@@ -41,11 +16,31 @@ const IndexContextProvider = ({ children }) => {
         setIndexData(data.results);
       })
       .catch((error) => console.log(error));
+    setIndexOrder(
+      JSON.parse(localStorage.getItem(INDEX_ORDER_LOCAL_STORAGE_KEY)) ??
+        IndexOrderDefault
+    );
   }, []);
+
+  // Wrapper method to save the new index order to the browser's local storage
+  // We're using JSON.stringify and JSON.parse because the localstorage API
+  // must accept a string.
+  const setIndexOrderAndPersist = (indexOrder) => {
+    localStorage.setItem(
+      INDEX_ORDER_LOCAL_STORAGE_KEY,
+      JSON.stringify(indexOrder)
+    );
+    setIndexOrder(indexOrder);
+  };
 
   return (
     <IndexContext.Provider
-      value={{ indexOrder, setIndexOrder, indexData, setIndexData }}
+      value={{
+        indexOrder,
+        setIndexOrder: setIndexOrderAndPersist,
+        indexData,
+        setIndexData,
+      }}
     >
       {children}
     </IndexContext.Provider>
---
+++ src/constants/IndexConstants.js
@@ -0,0 +1,32 @@
+export const IndexOrderDefault = [
+  {
+    symbol: "GSPC",
+    country: "us",
+  },
+  {
+    symbol: "DJI",
+    country: "us",
+  },
+  {
+    symbol: "FTSE",
+    country: "uk",
+  },
+  {
+    symbol: "STOXX50E",
+    country: "eu",
+  },
+  {
+    symbol: "99001.SZ",
+    country: "cn",
+  },
+  {
+    symbol: "HSI",
+    country: "hk",
+  },
+  {
+    symbol: "N225",
+    country: "jp",
+  },
+];
+
+export const INDEX_ORDER_LOCAL_STORAGE_KEY = "IndexOrder";
